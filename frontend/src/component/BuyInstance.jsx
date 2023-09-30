de7dae420959cff582d4cf26b28df8ad69bbf90a
trilochanb <trilochanbhusal000@gmail.com>
2023-10-01 05:19:42 +0545

    Update card for transfering product instance

--- frontend/src/component/BuyInstance.jsx
+++ frontend/src/component/BuyInstance.jsx
@@ -39,9 +39,9 @@ function MyVerticallyCenteredModal(props) {
                 <div className="card-header">
                     Featured
                 </div>
-                <div className="card-body">
-                    <h5 className="card-title">Special title treatment</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <div className="card-body">Transfer Instance
+                    <h5 className="card-title">Buy Payment</h5>
+                    <p className="card-text">Price: <span className='display-3'>{data['price']}</span></p>
                     <a href="#" className="btn btn-primary" onClick={handleBuyProduct}>Buy</a>
                 </div>
             </div>
